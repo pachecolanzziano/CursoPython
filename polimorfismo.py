97161ec04bb03221f64eee851241b94ca428f137
LucasLanzziano <pachecolanzziano@gmail.com>
2024-01-29 18:24:46 -0500

    polimorfismo

--- polimorfismo.py
+++ polimorfismo.py
@@ -6,10 +6,12 @@ class Coche():
 class Moto():
     def desplazamiento(self):
         print("Me desplazo utilizando dos ruedas")
+        
 class Camion():
     def desplazamiento(self):
         print("Me desplazo utilizando seis ruedas")
-        
+
+#esta es la funcion clave para que el concepto se de        
 def desplazamientoVehiculo(vehiculo):
     vehiculo.desplazamiento()
 
