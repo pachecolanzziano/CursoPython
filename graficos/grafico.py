e0713805da8388a18112052d46627d59f9417f64
Luis Carlos Pacheco Lanzziano <pachecolanzziano@gmail.com>
2024-02-07 23:30:53 -0500

    GUI 1
    graficos

---
+++ graficos/grafico.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+raiz = Tk()
+raiz.title("Ventana de Prueba")
+raiz.resizable(1,1)
+# raiz.iconbitmap("gato.ico")
+raiz.geometry("650x350")
+raiz.config(bg="blue")
+
+raiz.mainloop()
